6fed0b87478e8ec59fcc5e94a18c123c5dfbbbfc
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2022-12-28 18:05:39 +0300

    add fixing header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,34 @@
+import {useEffect} from "react";
+
 import Header from "../../components/Header/Header";
 import HeroMain from "../../components/HeroMain/HeroMain";
+import Products from "../../components/Products/Products";
 
 import './Home.scss';
 
 const Home = () => {
+  const HEADER_FIXED_CLASS = 'header--fixed';
+  
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if(entry.intersectionRatio >= 0.99) {
+        document.querySelector('.header').classList.remove(HEADER_FIXED_CLASS);
+      } else {
+        document.querySelector('.header').classList.add(HEADER_FIXED_CLASS);
+      }
+    })
+  }, { threshold: 0.99 })
+
+  useEffect(() => {
+    observer.observe(document.querySelector('.hero-main'));
+  })
+
   return (
     <>
       <Header />
       <main>
         <HeroMain />
+        <Products />
       </main>
     </>
   );
