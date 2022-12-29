9916e00b4d4b6c4c862c9e94ad3333817fbf8601
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2022-12-29 12:56:46 +0300

    replace header link to link component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,11 @@ import {Link} from "react-router-dom";
 
 import {BASKET_ROUTE} from "../../utils/consts";
 
-import {ReactComponent as Phone} from "../../assets/images/svg/phone.svg";
+import {ReactComponent as PhoneIcon} from "../../assets/images/svg/phone.svg";
 
 import Logo from "../Logo/Logo";
 import Nav from "../Navigation/Nav";
+import Phone from "../Phone/Phone"
 
 import './Header.scss';
 
@@ -17,9 +18,9 @@ const Header = () => {
         <Nav />
         <div className="header-contacts">
           <button className="btn-reset header-contacts__btn" aria-label="Заказать обратный звонок">
-            <Phone className="header-contacts__icon" />
+            <PhoneIcon className="header-contacts__icon" />
           </button>
-          <a href="tel:+74958235412" className="animation-link header-contacts__link">+7 (495) 823-54-12</a>
+          <Phone />
         </div>
         <Link to={BASKET_ROUTE} className="header__busket" title="Открыть корзину"></Link>
       </div>
