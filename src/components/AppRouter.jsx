96f3c79516fa344d8c09473a79e0322afa54d3d2
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2022-12-30 14:22:33 +0300

    router refactor

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -7,7 +7,7 @@ import {ROUTES} from "../routes";
 const AppRouter = () => {
   return (
     <Routes>
-      {ROUTES.map(({path, element}) => <Route key={path} path={path} element={element} /> )}}
+      {ROUTES.map(({path, element}) => <Route key={path} path={path} element={element} /> )}
 
       <Route
         element={<Navigate to={HOME_ROUTE} />}
