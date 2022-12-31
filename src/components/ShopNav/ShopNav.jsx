2e1c39fb926f457389a38934051ec545a61aa1aa
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2022-12-31 17:47:07 +0300

    add nav of shop

---
+++ src/components/ShopNav/ShopNav.jsx
@@ -0,0 +1,31 @@
+import {NavLink} from 'react-router-dom';
+
+import data from './data';
+
+import './ShopNav.scss';
+
+const ShopNav = () => {
+  const setClasses = ({isActive}) => {
+    const ACTIVE_CLASS_NAME = 'shop-nav__link--current';
+
+    const isActiveClass = isActive ? ' ' + ACTIVE_CLASS_NAME : '';
+
+    return "text-md shop-nav__link" + isActiveClass;
+  };
+  
+  const list = data.map(({link, name}) => {
+    return (
+      <li className="shop-nav__item" key={link}>
+        <NavLink to={link} className={setClasses} end>{name}</NavLink>
+      </li>
+    )
+  })
+  
+  return (
+    <ul className="shop-nav">
+      {list}    
+    </ul>
+  )
+}
+
+export default ShopNav
---
+++ src/components/ShopNav/data.js
@@ -0,0 +1,24 @@
+import {SHOP_ROUTE} from '../../utils/consts';
+
+export default [
+  {
+    link: SHOP_ROUTE,
+    name: "Все",
+  },
+  {
+    link: SHOP_ROUTE + '/t-shirt',
+    name: "Футболки",
+  },
+  {
+    link: SHOP_ROUTE + '/swimsuit',
+    name: "Купальники",
+  },
+  {
+    link: SHOP_ROUTE + '/sweatshirt',
+    name: "Свитшоты",
+  },
+  {
+    link: SHOP_ROUTE + '/hoody',
+    name: "Толстовки",
+  },
+]
