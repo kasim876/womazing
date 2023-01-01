edce359f1fb683b43562da45479896dbc1009fa1
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-01 20:48:49 +0300

    edit component product item

--- src/components/ProductsItem/ProductsItem.jsx
+++ src/components/ProductsItem/ProductsItem.jsx
@@ -8,7 +8,7 @@ const ProductItem = ({route, image, name, oldPrice, price}) => {
   return (
     <div className="product-item">
       <Link to={route} className="product-item__img-link">
-        <img src={image} alt="" className="product-item__img" />
+        <img src={image} alt="Фотография товара" width="350" height="478" className="product-item__img" />
       </Link>
       <Link to={route} className="animation-link product-item__name-link">
         <h3 className="product-item__name">{name}</h3>
