c123233e2ea97aab39c0687320d6ffdd2e4cf927
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-01 22:55:40 +0300

    add shop content

---
+++ src/components/ShopContent/ShopContent.jsx
@@ -0,0 +1,35 @@
+import {PRODUCT_ROUTE} from '../../utils/consts';
+
+import Spinner from '../Spinner/Spinner';
+import ShopNav from '../ShopNav/ShopNav';
+import ProductsItem from '../ProductsItem/ProductsItem';
+import ProductsList from '../ProductsList/ProductsList';
+
+import './ShopContent.scss';
+
+const ShopContent = ({products, isLoading}) => {
+  const items = products.map(({id, img, name, oldPrice, price}) => {
+    return (
+      <li key={name}>
+        <ProductsItem
+          route={PRODUCT_ROUTE + '/' + id}
+          image={process.env.REACT_APP_API_URL + img}
+          name={name}
+          oldPrice={oldPrice && '$' + oldPrice}
+          price={'$' + price}
+        />
+      </li>
+    )
+  })
+  
+  return (
+    <section className="shop-content">
+      <div className="container">
+        <ShopNav />
+        {isLoading ? <Spinner /> : <ProductsList items={items} />}
+      </div>
+    </section>
+  )
+}
+
+export default ShopContent
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,21 @@
-import {useEffect} from "react";
+import {useState, useEffect} from "react";
+import {useLocation} from 'react-router-dom';
+import {observer} from "mobx-react-lite";
+import axios from 'axios';
+
+import loading from "../store/loading";
 
 import Hero from "../components/Hero/Hero";
+import ShopContent from "../components/ShopContent/ShopContent"
 
-const Shop = () => {
-  const HEADER_FIXED_CLASS = 'header--fixed';
+const Shop = observer(() => {
+  const location = useLocation();
+  const [products, setProducts] = useState([]);
+
   
   const observer = new IntersectionObserver((entries, observer) => {
+    const HEADER_FIXED_CLASS = 'header--fixed';
+
     entries.forEach(entry => {
       if(entry.intersectionRatio >= 0.99) {
         document.querySelector('.header').classList.remove(HEADER_FIXED_CLASS);
@@ -23,11 +33,40 @@ const Shop = () => {
     return () => observer.unobserve(hero)
   }, [])
 
+  useEffect(() => {
+    const CancelToken = axios.CancelToken;
+    const source = CancelToken.source();
+    
+    loading.setIsLoading(true);
+
+    axios
+      .get(process.env.REACT_APP_API_URL + 'product', {
+        cancelToken: source.token,
+        params: {
+          category: document.querySelector('.shop-nav__link--current').dataset.category,
+        }
+      })
+      .then((res) => {
+        setProducts(res.data);
+      })
+      .then(() => {
+        loading.setIsLoading(false);
+      })
+      .catch(function(error) {
+        if (axios.isCancel(error)) {
+          return
+        }
+      })
+
+    return () => source.cancel()
+  }, [location.pathname]);
+
   return (
     <main>
       <Hero />
+      <ShopContent products={products} isLoading={loading.isLoading} />
     </main>
   );
-};
+});
 
 export default Shop;
