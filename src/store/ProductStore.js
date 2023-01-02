b98bf54de7458ef5c4af2db945b145fe42164073
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-02 17:46:56 +0300

    add product store

---
+++ src/store/ProductStore.js
@@ -0,0 +1,64 @@
+import {makeAutoObservable} from 'mobx';
+
+class ProductStore {
+  constructor() {
+    this._loading = false;
+    this._currentType = {};
+    this._products = [];
+    this._totalCount = 0;
+    this._page = 1;
+    this._limit = 9;
+    makeAutoObservable(this);
+  }
+
+  setLoading(bool) {
+    this._loading = bool;
+  }
+
+  setCurrentType(type) {
+    this._page = 1;
+    this._currentType = type;
+  }
+
+  setProducts(products) {
+    this._products = products;
+  }
+
+  setTotalCount(count) {
+    this._totalCount = count;
+  }
+
+  setPage(page) {
+    this._page = page;
+  }
+
+  setLimit(limit) {
+    this._limit = limit;
+  }
+
+  get loading() {
+    return this._loading;
+  }
+
+  get currentType() {
+    return this._currentType;
+  }
+
+  get products() {
+    return this._products;
+  }
+
+  get totalCount() {
+    return this._totalCount;
+  }
+
+  get page() {
+    return this._page;
+  }
+
+  get limit() {
+    return this._limit;
+  }
+}
+
+export default new ProductStore();
