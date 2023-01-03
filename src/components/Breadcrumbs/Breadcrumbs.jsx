00e3361ede9b5c0b4ad057004657e4324e2d08a5
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-03 22:51:28 +0300

    update breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,11 +1,19 @@
+import {Link} from 'react-router-dom';
+
+import {HOME_ROUTE} from '../../utils/consts';
+
 import './Breadcrumbs.scss';
 
 const Breadcrumbs = ({list}) => {
   return (
     <ul className="breadcrumbs">
       {
-        list.map(item => <li className="text-md breadcrumbs__item" key={item}>{item}</li>
-        )}
+        list.map(item => (
+          <li className="text-md breadcrumbs__item" key={item}>
+            <Link to={HOME_ROUTE} className="breadcrumbs__link">{item}</Link>
+          </li>
+        ))
+      }
     </ul>
   );
 };
