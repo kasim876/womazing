c1dd9f6257f9493509ac70bfd5d188881a84313b
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-07 23:48:42 +0200

    update breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -8,8 +8,8 @@ const Breadcrumbs = ({list}) => {
   return (
     <ul className="breadcrumbs">
       {
-        list.map(item => (
-          <li className="text-md breadcrumbs__item" key={item}>
+        list.map((item, index) => (
+          <li className="text-md breadcrumbs__item" key={index}>
             <Link to={HOME_ROUTE} className="breadcrumbs__link">{item}</Link>
           </li>
         ))
