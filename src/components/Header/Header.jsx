2287ee4aaf53fd3289dc011d19b4ff5dec7c8987
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-08 15:51:04 +0200

    add basket counter in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import {Link} from 'react-router-dom';
+import {observer} from 'mobx-react-lite';
 
 import {BASKET_ROUTE} from '../../utils/consts';
 
-import {ReactComponent as PhoneIcon} from '../../assets/images/svg/phone.svg';
+import basket from '../../store/BasketStore';
 
+import {ReactComponent as PhoneIcon} from '../../assets/images/svg/phone.svg';
+import {ReactComponent as BasketIcon} from '../../assets/images/svg/bag.svg';
 import Logo from '../Logo/Logo';
 import Nav from '../Navigation/Nav';
 import Phone from '../Phone/Phone';
 
 import './Header.scss';
 
-const Header = () => {
+const Header = observer(() => {
   return (
     <header className="header">
       <div className="container header__container">
@@ -22,10 +25,13 @@ const Header = () => {
           </button>
           <Phone />
         </div>
-        <Link to={BASKET_ROUTE} className="header__busket" title="Открыть корзину"></Link>
+        <Link to={BASKET_ROUTE} className="header__basket-wrap" title="Открыть корзину">
+          <BasketIcon className="header__basket" />
+          <input type="text" className="input-reset header__basket-count" value={basket.basket.length} readOnly />
+        </Link>
       </div>
     </header>
   );
-};
+});
 
 export default Header;
