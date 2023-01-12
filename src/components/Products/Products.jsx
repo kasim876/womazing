4ef0df4e7b109d46bf70a07b9069ce3ed11a7689
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-12 17:59:11 +0200

    refactor product section

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom';
 
-import {PRODUCT_ROUTE} from '../../utils/consts';
+import {PRODUCT_ROUTE, SHOP_ROUTE} from '../../utils/consts';
 
 import productImage1 from '../../assets/images/productImage1.jpg';
 import productImage2 from '../../assets/images/productImage2.jpg';
@@ -15,34 +15,34 @@ const Products = () => {
         <h2 className="title products__title">Новая коллекция</h2>
         <ul className="products__list">
           <li className="products__item">
-            <Link to={PRODUCT_ROUTE} className="products__img-link">
+            <Link to={PRODUCT_ROUTE + '/1'} className="products__img-link">
               <img src={productImage1} alt="Девушка в футболке USA" className="products__img" />
             </Link>
-            <Link to={PRODUCT_ROUTE} className="animation-link products__name-link">
+            <Link to={PRODUCT_ROUTE + '/1'} className="animation-link products__name-link">
               <h3 className="products__name">Футболка USA</h3>
             </Link>
             <span className="products__price"><del>$229</del>$129</span>
           </li>
           <li className="products__item">
-            <Link to={PRODUCT_ROUTE} className="products__img-link">
+            <Link to={PRODUCT_ROUTE + '/9'}  className="products__img-link">
               <img src={productImage2} alt="Девушка в купальнике Glow" className="products__img" />
             </Link>
-            <Link to={PRODUCT_ROUTE} className="animation-link products__name-link">
+            <Link to={PRODUCT_ROUTE + '/9'} className="animation-link products__name-link">
               <h3 className="products__name">Купальник Glow</h3>
             </Link>
             <span className="products__price">$129</span>
           </li>
           <li className="products__item">
-            <Link to={PRODUCT_ROUTE} className="products__img-link">
+            <Link to={PRODUCT_ROUTE + '/12'} className="products__img-link">
               <img src={productImage3} alt="Девушка в свитшоте Sweet Shot" className="products__img" />
             </Link>
-            <Link to={PRODUCT_ROUTE} className="animation-link products__name-link">
+            <Link to={PRODUCT_ROUTE + '/12'} className="animation-link products__name-link">
               <h3 className="products__name">Свитшот Sweet Shot</h3>
             </Link>
             <span className="products__price">$129</span>
           </li>
         </ul>
-        <button className="btn-reset products__btn">Открыть магазин</button>
+        <Link to={SHOP_ROUTE} className="btn-reset products__btn">Открыть магазин</Link>
       </div>
     </section>
   );
