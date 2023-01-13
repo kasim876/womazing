f005f52c87b65fbd79a9146d09511af503a8a846
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-13 10:46:46 +0200

    refactor basket

--- src/components/BasketContent/BasketContent.jsx
+++ src/components/BasketContent/BasketContent.jsx
@@ -10,33 +10,33 @@ const BasketContent = observer(() => {
   return (
     <section className="basket">
       <div className="container">
-        <table className="basket-table">
-          <thead className="basket-table__header">
-            <tr className="basket-table__row">
-              <th>Товар</th>
-              <th>Цена</th>
-              <th>Количество</th>
-              <th>Всего</th>
-            </tr>
-          </thead>
-          {
-            basketStore.products.length ? 
-              <>
+        {
+          basketStore.products.length ? 
+            <>
+              <table className="basket-table">
+                <thead className="basket-table__header">
+                  <tr className="basket-table__row">
+                    <th>Товар</th>
+                    <th>Цена</th>
+                    <th>Количество</th>
+                    <th>Всего</th>
+                  </tr>
+                </thead>
                 <tbody className="basket-table__body">
                   <BasketList />
                 </tbody>
-                <div className="basket__bottom">
-                  <div className="basket__price-wrap">
-                    <h3 className="basket__price-caption">Итого:</h3>
-                    <span className="basket__price">{basketStore.totalPrice() + '₽'}</span>
-                  </div>
-                  <button className="btn-reset btn-green basket__btn">Оформить заказ</button>
+              </table>
+              <div className="basket__bottom">
+                <div className="basket__price-wrap">
+                  <h3 className="basket__price-caption">Итого:</h3>
+                  <span className="basket__price">{basketStore.totalPrice() + '₽'}</span>
                 </div>
-              </>
-              :
-              <h2 align="center" style={{'margin-top': '50px'}}>В корзине нет товаров</h2>
-          }
-        </table>
+                <button className="btn-reset btn-green basket__btn">Оформить заказ</button>
+              </div>
+            </>
+            :
+            <h2 align="center">В корзине нет товаров</h2>
+        }
       </div>
     </section>
   );
