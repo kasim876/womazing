444da2fba15b9bdc4e3d26048d65635826800d20
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-15 14:43:44 +0200

    main hero mobile adaptive

--- src/components/shared/hero-main/hero-slider/HeroSlider.jsx
+++ src/components/shared/hero-main/hero-slider/HeroSlider.jsx
@@ -23,18 +23,19 @@ const HeroSlider = () => {
         bulletActiveClass: 'bullet-current',
         clickable: true,
       }}
+      className={styles.root}
     >
-      <SwiperSlide>
+      <SwiperSlide className={styles.slide}>
         <h1 className={styles.title}>Новые поступления в этом сезоне</h1>
         <p className={styles.descr}>Утонченные сочетания и бархатные оттенки - вот то, что вы искали в этом сезоне. Время исследовать.</p>
       </SwiperSlide>
-      <SwiperSlide>
+      <SwiperSlide className={styles.slide}>
         <h1 className={styles.title}>Что-то новенькое. Мы заждались тебя.</h1>
-        <p className="hero-main__descr">Надоело искать себя в сером городе? Настало время новых идей, свежих красок и вдохновения с Womazing!</p>
+        <p className={styles.descr}>Надоело искать себя в сером городе? Настало время новых идей, свежих красок и вдохновения с Womazing!</p>
       </SwiperSlide>
-      <SwiperSlide>
+      <SwiperSlide className={styles.slide}>
         <h1 className={styles.title}>Включай новый сезон с WOMAZING</h1>
-        <p className="hero-main__descr">Мы обновили ассортимент - легендарные коллекции и новинки от отечественных дизайнеров</p>
+        <p className={styles.descr}>Мы обновили ассортимент - легендарные коллекции и новинки от отечественных дизайнеров</p>
       </SwiperSlide>
     </Swiper>
   );
