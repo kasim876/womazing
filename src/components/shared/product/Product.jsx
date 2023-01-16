6a2a801cd28d0c369465d913e2b6ceb7b904afbb
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-16 14:45:20 +0200

    edit product card

--- src/components/shared/product/Product.jsx
+++ src/components/shared/product/Product.jsx
@@ -6,15 +6,13 @@ const Product = ({route, image, name, oldPrice, price}) => {
   oldPrice = oldPrice && <del>{oldPrice}</del>;
 
   return (
-    <div className={styles.root}>
-      <Link to={route} className={styles['img-wrap']}>
+    <Link to={route} className={styles.root}>
+      <div className={styles['img-wrap']}>
         <img src={image} alt="" width="350" height="478" className={styles.img} />
-      </Link>
-      <Link to={route} className={`animation-link ${styles['name-wrap']}`}>
-        <h3 className={styles.name}>{name}</h3>
-      </Link>
+      </div>
+      <h3 className={styles.name}>{name}</h3>
       <span className={`text-md ${styles.price}`}>{oldPrice}{price}</span>
-    </div>
+    </Link>
   );
 };
 
